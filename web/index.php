2db65fd9825e826815ad0818a28c39d802435fcd
Jorge-mendez2000 <jorge.mendez.ortiz.itics.2000@gmail.com>
2023-08-09 11:35:36 -0600

    Parrafo agregado

--- web/index.php
+++ web/index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>DocumDocumento sin titulo </title>
+    <link rel="stylesheet" href="../css/estilos.css" type="text/css">
+
+
 </head>
 <body>
-    
+    <h1>Proyecto 1 git</h1>
+    <p>Segundo comit</p>
 </body>
 </html>
