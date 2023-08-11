a1573b143a05a60b8ddd7ba57bb7e8c7a5ef79c0
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 <jorge.mendez.ortiz.itics.2000@gmail.com>
2023-08-10 19:04:08 -0600

    Saludo agregado js

--- web/index.php
+++ web/index.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DocumDocumento sin titulo </title>
     <link rel="stylesheet" href="../css/estilos.css" type="text/css">
-
+    <script src=../js/javascrip.js></script>
 
 </head>
 <body>
     <h1>Proyecto 1 git</h1>
+    <input type="button" onclick="saludo()" value="Dale ya">
     <h1>Proyecto 1 git</h1> 
     <h1>Proyecto 1 git</h1>
     <h1>Proyecto 1 git</h1>
